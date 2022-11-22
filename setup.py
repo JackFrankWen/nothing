1ddb46ec09abae896db10d05787d44fbdd8168d8
baiqiu.wsn <baiqiu.wsn@raycloud.com>
2022-11-22 10:13:50 +0800

    feat: 初始化

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nothing',
+    version='',
+    packages=['GR86', 'GR86.chassis'],
+    url='',
+    license='',
+    author='admin',
+    author_email='',
+    description=''
+)
